9fc07cfd81213ce788eae64f60d27e65fb9af052
ggpsgeorge <ggpsgeorge@gmail.com>
2017-11-14 08:38:22 -0200

    ponto a ponto exem

--- exem.cc
+++ exem.cc
@@ -1,49 +1,59 @@
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
+#include "ns3/csma-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
 
 using namespace ns3;
 
+NS_LOG_COMPONENT_DEFINE("Exem1");
+
 int main(int argc, char *argv[]){
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
 
-	CommandLine cmd;
-  	cmd.Parse (argc, argv);
+  Time::SetResolution (Time::NS);
+  LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+  LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+
+  NodeContainer nodes;
+  nodes.Create(2);
+
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
+  pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+  NetDeviceContainer devices;
+  devices = pointToPoint.Install(nodes);
+
+  InternetStackHelper stack;
+  stack.Install(nodes);
+
+  Ipv4AddressHelper address;
+  address.SetBase("10.1.1.0","255.255.255.0");
+
+  Ipv4InterfaceContainer interfaces = address.Assign(devices);
   
-  	Time::SetResolution (Time::NS);
-  	LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  	LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+  UdpEchoServerHelper echoServer(9); //Escolhendo porta
 
-  	NodeContainer nodes;
-  	nodes.Create(2);
+  ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
+  serverApps.Start(Seconds(1));
+  serverApps.Stop(Seconds(10));
 
-  	PointToPointHelper pointToPoint;
-  	pointToPoint.SetDeviceAttribute("DataRate", StringValue ("10Mbps"));
-  	pointToPoint.SetChannelAttribute("Delay", StringValue ("1ms"));
+  UdpEchoClientHelper echoClient(interfaces.GetAddress(1), 9);
+  echoClient.SetAttribute("MaxPackets", UintegerValue(3));
+  echoClient.SetAttribute("Interval", TimeValue(Seconds(1)));
+  echoClient.SetAttribute("PacketSize", UintegerValue(1024));
 
-  	NetDeviceContainer devices;
-  	devices = pointToPoint.Install(nodes);
+  ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
+  clientApps.Start(Seconds(3));
+  clientApps.Stop(Seconds(9));
 
-  	InternetStackHelper stack;
-  	stack.Install(nodes);
+  Simulator::Run();
+  Simulator::Destroy();
 
-  	Ipv4AddressHelper adress;
-  	adress.SetBase("10.1.1.0","255.255.255.0");
 
-  	Ipv4InterfaceContainer interfaces = adress.Assign(devices);
-
-  	UdpEchoServerHelper echoServer(9);
-
-  	ApplicationContainer serverApps = echoServer.Install(nodes.Get(2));
-  	serverApps.Start(Seconds(1));
-  	serverApps.Stop(Seconds(10));
-
-  	
-
-  	Simulator::Run();
-  	Simulator::Destroy();
-
-  	return 0;
-
+  return 0;
 }
