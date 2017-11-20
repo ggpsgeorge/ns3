5307b03efb6896706035db913dfbde7759f3cdc3
ggpsgeorge <ggpsgeorge@gmail.com>
2017-11-20 18:24:42 -0200

    Erro Ontime

--- exemStar.cc
+++ exemStar.cc
@@ -5,7 +5,6 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/point-to-point-layout-module.h"
-
 /*Topologia Estrela
 			 	n2
 	   p2p	   /
@@ -17,44 +16,61 @@ n4(server) fora da topologia
 */
 
 NS_LOG_COMPONENT_DEFINE("exemStar");
+using namespace ns3;
 
 int main(int argc, char *argv[]){
 
-uint32_t nNodes = 5
+	uint32_t nNodes = 5;
 
-PointToPointHelper p2pnode;
-p2pnode.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-p2pnode.SetChannelAttribute("Delay", StringValue("2ms"));
+	Config::SetDefault("ns3::OnOffApplication::PacketSize", UintegerValue(137));
+	Config::SetDefault("ns3::OnOffApplication::DataRate", StringValue("14kb/s"));
 
-//Criando a a topologia
-PointToPointStarHelper star (nNodes, p2pnode);
+	PointToPointHelper p2pnode;
+	p2pnode.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+	p2pnode.SetChannelAttribute("Delay", StringValue("2ms"));
 
-//Instalando bagulhos em cada no
-InternetStackHelper internet;
-star.InstallStack(internet);
+	//Criando a a topologia
+	PointToPointStarHelper star (nNodes, p2pnode);
 
-star.AssignIpv4Addresses (Ipv4AddressHelper ("10.1.1.0", "255.255.255.0"));
+	//Instalando bagulhos em cada no
+	InternetStackHelper internet;
+	star.InstallStack(internet);
 
-//Criar o hub para a topologia
-uint32_t porta = 8000;
-Address hubLocalAdress (InternetSocketAdress (Ipv4Address::GetAny(), porta));
-PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", hubLocalAdress);
-ApplicationContainer hupApp = packetSinkHelper.Install(star.GetHub);
+	star.AssignIpv4Addresses (Ipv4AddressHelper ("10.1.1.0", "255.255.255.0"));
 
-hubApp.Start(Seconds(1.0); 
-hubApp.Stop(Seconds(10.0));
+	//Criar o hub para a topologia
+	uint16_t porta = 8000;
+	Address hubLocalAdress (InetSocketAddress (Ipv4Address::GetAny(), porta));
+	PacketSinkHelper packetSinkHelper ("ns3::TcpSocketFactory", hubLocalAdress);
+	ApplicationContainer hubApp = packetSinkHelper.Install(star.GetHub());
 
-ApplicationContainer nodesApp;
+	hubApp.Start(Seconds(1.0)); 
+	hubApp.Stop(Seconds(10.0));
 
-for(uint32_t i = 0; i < star.SpokeCount(), ++i){
-	AddressValue remoteAddress (InetSocketAdress (star.GetHubIpv4Address(i), porta))
-}
+	//Fora da topologia se manda pacotes para o hub da estrela
+	OnOffHelper onOffHelper ("ns3::TcpSocketFactory", Address());
+	onOffHelper.SetAttribute("Ontime", StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+	onOffHelper.SetAttribute("Offtime", StringValue("ns3::ConstantRandomVariable[Constant=0]"));
 
-nodesApp.Start(Seconds(1.0));
-nodesApp.Stop(Seconds(10.0));
+	ApplicationContainer nodesApp;
 
-//Rotear entre os nos da estrela
-Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+	for(uint32_t i = 0; i < star.SpokeCount(); ++i){
+		AddressValue remoteAddress (InetSocketAddress (star.GetHubIpv4Address(i), porta));
+		onOffHelper.SetAttribute("Remote", remoteAddress);
+		nodesApp.Add(onOffHelper.Install(star.GetSpokeNode(i)));
+
+	}
+
+	nodesApp.Start(Seconds(1.0));
+	nodesApp.Stop(Seconds(10.0));
+
+	//Rotear entre os nos da estrela
+	Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+	p2pnode.EnablePcapAll("star");
+
+	Simulator::Run();
+	Simulator::Destroy();
 
 	return 0;
 }
